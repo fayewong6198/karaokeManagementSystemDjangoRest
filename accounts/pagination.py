08c10ba1bf98a3a79ccd0288889522d774ff124a
faye wong <trandaosimanh@gmail.com>
2020-05-22 21:17:34 +0700

    change page size from 3 to 10

--- accounts/pagination.py
+++ accounts/pagination.py
@@ -10,7 +10,7 @@ class LargeResultsSetPagination(PageNumberPagination):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
